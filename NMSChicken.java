e1a088c6f2d57ddac4476d9dd17f85bffa66433d
Lioche <games@lioche.be>
2015-10-21 19:52:52 +0200

    Update NMSChicken.java

--- NMSChicken.java
+++ NMSChicken.java
@@ -1,10 +1,8 @@
-package be.lioche.compact.obj;
+package be.lioche.api.nms;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.UUID;
 
 import net.minecraft.server.v1_8_R2.Entity;
 import net.minecraft.server.v1_8_R2.EntityChicken;
@@ -19,6 +17,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_8_R2.CraftWorld;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
@@ -26,94 +25,153 @@ import org.bukkit.inventory.ItemStack;
 
 import be.lioche.api.enums.Particles;
 import be.lioche.api.enums.Symboles;
+import be.lioche.api.main.Main;
+import be.lioche.api.utils.Others;
 
 public class NMSChicken extends EntityChicken implements Listener{
 
 	public static List<ItemStack> items = new ArrayList<>();
+
+	public static List<ItemStack> armors = new ArrayList<>();
+	public static List<ItemStack> weapons = new ArrayList<>();
 
 	Player owner;
 	Location loc;
 
 	NMSChicken chick;
 	Inventory chickenInv;
-	UUID chickUUID;
 
 	public NMSChicken(World world, Player player) {
 		super(((CraftWorld) world).getHandle());
 
-		List<?> goalB = (List<?>)getPrivateField("b", PathfinderGoalSelector.class, goalSelector); goalB.clear();
-		List<?> goalC = (List<?>)getPrivateField("c", PathfinderGoalSelector.class, goalSelector); goalC.clear();
-		List<?> targetB = (List<?>)getPrivateField("b", PathfinderGoalSelector.class, targetSelector); targetB.clear();
-		List<?> targetC = (List<?>)getPrivateField("c", PathfinderGoalSelector.class, targetSelector); targetC.clear();
+		List<?> goalB = (List<?>)EntityTypes.getPrivateField("b", PathfinderGoalSelector.class, goalSelector); goalB.clear();
+		List<?> goalC = (List<?>)EntityTypes.getPrivateField("c", PathfinderGoalSelector.class, goalSelector); goalC.clear();
+		List<?> targetB = (List<?>)EntityTypes.getPrivateField("b", PathfinderGoalSelector.class, targetSelector); targetB.clear();
+		List<?> targetC = (List<?>)EntityTypes.getPrivateField("c", PathfinderGoalSelector.class, targetSelector); targetC.clear();
 
 
 		this.goalSelector.a(0, new PathfinderGoalFloat(this));
 		this.goalSelector.a(6, new PathfinderGoalLookAtPlayer(this,EntityHuman.class, 6.0F));
 
 		this.owner = player;
-		setup();
+		this.chick = this;
+		this.uniqueID = this.owner.getUniqueId();
+		this.loc = this.owner.getLocation().add(0, 20, 0);
+	}
+
+	@Override
+	public void makeSound(String s, float f, float f1) {
+		if (!this.R()) {
+			this.world.makeSound(this, "random.levelup", 1, 1);
+			Particles.sendParticle(EnumParticle.HEART, new Location(this.world.getWorld(), this.locX, this.locY+1, this.locZ), 0, 0, 0, 1, 10);
+		}
 	}
 
 	public void setup(){
-		this.chickenInv = Bukkit.getServer().createInventory(owner, 27, Symboles.ARROW_LITTLE_RIGHT.get()+" Sponsor de "+this.owner.getName()+ " "+Symboles.ARROW_LITTLE_LEFT.get());
-		this.chickUUID = this.owner.getUniqueId();
-		this.chick = this;
-		this.loc = this.owner.getLocation().add(1, 20, 0);
-
-		this.setCustomName(Symboles.ARROW_LITTLE_RIGHT.get()+" Sponsor de "+this.owner.getName()+ " "+Symboles.ARROW_LITTLE_LEFT.get());
+		this.setCustomName("§c"+Symboles.ARROW_LITTLE_RIGHT.get()+"§f Sponsor de "+this.owner.getName()+ "§c "+Symboles.ARROW_LITTLE_LEFT.get());
 		this.uniqueID = this.owner.getUniqueId();
+		this.expToDrop = 0;
+		this.fireProof = true;
+		this.noclip = true;
 		this.setCustomNameVisible(true);
-		setupItems();
+		this.setAbsorptionHearts(999);
+		this.dropDeathLoot(false, 0);
+		this.dropEquipment(false, 0);
 	}
 
-	private void setupItems(){
+	public void setupItems(){
+		this.chickenInv = Bukkit.getServer().createInventory(owner, 27, Symboles.ARROW_LITTLE_RIGHT.get()+" Sponsor de "+this.owner.getName()+ " "+Symboles.ARROW_LITTLE_LEFT.get());
+
 		Random r = new Random();
-		int nbr = r.nextInt(5)+3;
-		int ri;
-		int po;
-		
-		ItemStack i1 = new ItemStack(Material.DIAMOND_HELMET);
-		ItemStack i2 = new ItemStack(Material.DIAMOND_CHESTPLATE);
-		ItemStack i3 = new ItemStack(Material.DIAMOND_LEGGINGS);
-		ItemStack i4 = new ItemStack(Material.DIAMOND_BOOTS);
-		
-		ItemStack i5 = new ItemStack(Material.BOW);
-		ItemStack i6 = new ItemStack(Material.BREAD, nbr*nbr);
-		ItemStack i7 = new ItemStack(Material.LOG, nbr*nbr);
-		ItemStack i8 = new ItemStack(Material.EXP_BOTTLE, nbr*nbr);
-		
-		
-		items.add(i1);
-		items.add(i2);
-		items.add(i3);
-		items.add(i4);
-		items.add(i5);
-		items.add(i6);
-		items.add(i7);
-		items.add(i8);
+		int nbr = r.nextInt(3)+5;
+		int slot;
 
+		int isenchant;
+		int whatenchant;
+		int lvlenchant;
+		int selected;
+
+		ItemStack dh = new ItemStack(Material.DIAMOND_HELMET);
+		ItemStack dc = new ItemStack(Material.DIAMOND_CHESTPLATE);
+		ItemStack dl = new ItemStack(Material.DIAMOND_LEGGINGS);
+		ItemStack db = new ItemStack(Material.DIAMOND_BOOTS);
+		
+		armors.add(dh);
+		armors.add(dc);
+		armors.add(dl);
+		armors.add(db);
+		
+		ItemStack bow = new ItemStack(Material.BOW);
+		ItemStack ds = new ItemStack(Material.DIAMOND_SWORD); 
+		
+		weapons.add(bow);
+		weapons.add(ds);
+		
+		ItemStack bread = new ItemStack(Material.BREAD, nbr*(nbr/2)+((nbr*2)/3));
+		ItemStack log = new ItemStack(Material.LOG, nbr*(nbr/2)+((nbr*2)/3));
+		ItemStack exp = new ItemStack(Material.EXP_BOTTLE, nbr*(nbr/2)+((nbr*2)/3));
+		ItemStack arrow = new ItemStack(Material.ARROW, nbr+(nbr/2)+5);
+
+		items.add(bread);
+		items.add(log);
+		items.add(exp);
+		items.add(arrow);
+		
+		items.addAll(weapons);
+		items.addAll(armors);
+		
 		for(int i = 0; i < nbr; i++){
-			ri = r.nextInt(items.size());
-			po = r.nextInt(27);
-			if(!this.chickenInv.contains(items.get(ri))){
-				this.chickenInv.setItem(po, items.get(ri));
+
+			isenchant = r.nextInt(armors.size());
+			whatenchant = r.nextInt(4)+1;
+			lvlenchant = r.nextInt(3)+1;
+
+			selected = r.nextInt(items.size());
+			slot = r.nextInt(27);
+
+			ItemStack item = items.get(selected);
+
+			if(!this.chickenInv.contains(item)){
+				if(armors.contains(item)){
+					if(isenchant == armors.size()/2){
+						Others.addEnchant(item, Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+					}
+				}
+
+				if(weapons.contains(item)){
+					if(isenchant == weapons.size()/2){
+						if(item.getType().equals(Material.BOW)){
+							if(whatenchant == 1){
+								Others.addEnchant(item, Enchantment.ARROW_FIRE, lvlenchant);
+							}else if(whatenchant == 2){
+								Others.addEnchant(item, Enchantment.ARROW_KNOCKBACK, lvlenchant);
+							}else{
+								Others.addEnchant(item, Enchantment.ARROW_DAMAGE, lvlenchant);
+							}
+						}else{
+							Others.addEnchant(item, Enchantment.DAMAGE_ALL, lvlenchant);
+						}
+					}
+				}
+
+				this.chickenInv.setItem(slot, item);
 			}
+
+
 		}
 	}
 
-	public static void spawnEntity(Entity entity, Location loc)
-	{
-		entity.setLocation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+	public static void spawnEntity(final Entity entity, Location loc)	{
+		entity.setLocation(loc.getX(), loc.getY(), loc.getZ(), -loc.getYaw(), -loc.getPitch());
 		((CraftWorld)loc.getWorld()).getHandle().addEntity(entity);
-		for(double t = 0; t < 10 * Math.PI; t += 0.39){
-			Particles.sendParticle(EnumParticle.FIREWORKS_SPARK, loc, Math.sin(t), t, Math.cos(t), 1, 20);
-		}
-	}
 
-	public String getUUID(){
-		return this.chickUUID.toString();
+		Bukkit.getScheduler().runTaskLater(Main.instance, new Runnable() {
+			public void run() {
+				entity.die();
+			}
+		}, 20*60L);
 	}
-	public Inventory getInventory(){
+	public Inventory getChickInventory(){
 		return this.chickenInv;
 	}
 	public Player getOwner(){
@@ -122,25 +180,15 @@ public class NMSChicken extends EntityChicken implements Listener{
 	public Location getLocation(){
 		return this.loc;
 	}
+	public void setInventory(Inventory inv){
+		this.chickenInv = inv;
+	}
 
-	public static Object getPrivateField(String fieldName, Class<?> clazz, Object object)
-	{
-		Field field;
-		Object o = null;
-		try
-		{
-			field = clazz.getDeclaredField(fieldName);
-			field.setAccessible(true);
-			o = field.get(object);
-		}
-		catch(NoSuchFieldException e)
-		{
-			e.printStackTrace();
-		}
-		catch(IllegalAccessException e)
-		{
-			e.printStackTrace();
-		}
-		return o;
+	public void setNoClip(final boolean bool, long time){
+		Bukkit.getScheduler().runTaskLater(Main.instance, new Runnable() {
+			public void run() {
+				noclip = bool;
+			}
+		}, 10L*time);
 	}
 }

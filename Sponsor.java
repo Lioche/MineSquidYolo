4c733e2ba61b5e86c0925ba34cafc64a3b1c7816
Lioche <games@lioche.be>
2015-10-21 19:53:16 +0200

    Update Sponsor.java

--- Sponsor.java
+++ Sponsor.java
@@ -1,14 +1,15 @@
 package be.lioche.compact.cmd;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
+import be.lioche.api.nms.EntityTypes;
+import be.lioche.api.nms.NMSChicken;
 import be.lioche.compact.main.Main;
-import be.lioche.compact.obj.EntityTypes;
-import be.lioche.compact.obj.NMSChicken;
 
 public class Sponsor implements Listener, CommandExecutor{
 
@@ -22,31 +23,86 @@ public class Sponsor implements Listener, CommandExecutor{
 
 		if(sender instanceof Player){
 			Player p = (Player)sender;
+			NMSChicken nms = new NMSChicken(p.getWorld(), p);
+//			Location loc; 
+//			Block bl;
 			int min = Main.instance.getConfig().getInt(p.getUniqueId()+".sponsor");
 
-			if(Main.instance.getConfig().contains(p.getUniqueId().toString())){
-				if(p.hasPermission("lioche.sponsor")){
-					NMSChicken nms = new NMSChicken(p.getWorld(), p);
+			if(Main.instance.getConfig().contains(p.getUniqueId().toString())){				
+				if(min == 0){
+//					REMPLACE PAR LA FONCTION SETNOCLIP, TRAVERSE LES MURS.
+//					
+//					for(int b = (int) p.getLocation().getY(); b < p.getLocation().getY()+22; b++){
+//						loc = p.getLocation();
+//						loc.setY(b);
+//
+//						bl = loc.getBlock();
+//						if(bl.getType() != Material.AIR){
+//							p.sendMessage(Main.prefix+"Il ne doit pas y avoir de bloc au dessus de vous.");
+//							return false;
+//						}
+//					}
+					nms.setup();
+					nms.setNoClip(false, 17); 
 					EntityTypes.spawnEntity(nms, nms.getLocation());
+					
 					p.sendMessage(Main.prefix+"Ton sponsor arrive par les airs.");
+					Main.instance.getConfig().set(p.getUniqueId()+".sponsor", 30);
+					Main.instance.saveConfig();
+					launchCooldown(p);
+					
+					return true;
 				}else{
-					if(min == 0){
-						NMSChicken nms = new NMSChicken(p.getWorld(), p);
-						EntityTypes.spawnEntity(nms, nms.getLocation());
-						p.sendMessage(Main.prefix+"Ton sponsor arrive par les airs.");
-						Main.instance.getConfig().set(p.getUniqueId()+".sponsor", 0);
-						Main.instance.saveConfig();
-					}else{
-						p.sendMessage(Main.prefix + " Votre sponsor sera disponible dans: "+ min + " min.");
-					}
+					p.sendMessage(Main.prefix + "Votre sponsor sera disponible dans: "+ min + " min.");
+					return false;
 				}
 			}else{
-				Main.instance.getConfig().set(p.getUniqueId()+".sponsor", 0);
+//				REMPLACE PAR LA FONCTION SETNOCLIP, TRAVERSE LES MURS.
+//				
+//				for(int b = (int) p.getLocation().getY(); b < p.getLocation().getY()+22; b++){
+//					loc = p.getLocation();
+//					loc.setY(b);
+//
+//					bl = loc.getBlock();
+//					if(bl.getType() != Material.AIR){
+//						p.sendMessage(Main.prefix+"Il ne doit pas y avoir de bloc au dessus de vous.");
+//						return false;
+//					}
+//				}
+				nms.setup();
+				nms.setNoClip(false, 17); 
+				EntityTypes.spawnEntity(nms, nms.getLocation());
+				
+				p.sendMessage(Main.prefix+"Ton sponsor arrive par les airs.");
+				Main.instance.getConfig().set(p.getUniqueId()+".sponsor", 30);
 				Main.instance.saveConfig();
+				launchCooldown(p);
+				
+				return true;
 			}
 
 		}
 		return false;
 	}
 
+	public static void launchCooldown(final Player p){
+		if(Main.instance.getConfig().getInt(p.getUniqueId()+".sponsor") != 0){
+			final int c = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.instance, new Runnable() {
+				int min = Main.instance.getConfig().getInt(p.getUniqueId()+".sponsor");
+				public void run() {
+					if(min != 0){
+						min = min--;
+						Main.instance.getConfig().set(p.getUniqueId()+".sponsor", min--);
+						Main.instance.saveConfig();
+					}
+				}
+			}, 20*60L, 20*60L);
+
+			Bukkit.getScheduler().runTaskLater(Main.instance, new Runnable() {
+				public void run() {
+					Bukkit.getScheduler().cancelTask(c);
+				}
+			}, 20*60*30L);
+		}
+	}
 }

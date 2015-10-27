ed95f671ebc1e67d0fa73d3037928b716ce7d51c
Lioche <games@lioche.be>
2015-10-27 20:09:24 +0100

    Update NMSChicken.java

--- NMSChicken.java
+++ NMSChicken.java
@@ -22,6 +22,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MonsterEggs;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionType;
 
 import be.lioche.api.enums.Particles;
 import be.lioche.api.enums.Symboles;
@@ -34,6 +37,8 @@ public class NMSChicken extends EntityChicken implements Listener{
 
 	public static List<ItemStack> armors = new ArrayList<>();
 	public static List<ItemStack> weapons = new ArrayList<>();
+	public static List<ItemStack> potions = new ArrayList<>();
+	public static List<ItemStack> noDouble = new ArrayList<>();
 
 	Player owner;
 	Location loc;
@@ -79,11 +84,15 @@ public class NMSChicken extends EntityChicken implements Listener{
 		this.dropEquipment(false, 0);
 	}
 
+	@SuppressWarnings("deprecation")
 	public void setupItems(){
 		this.chickenInv = Bukkit.getServer().createInventory(owner, 27, Symboles.ARROW_LITTLE_RIGHT.get()+" Sponsor de "+this.owner.getName()+ " "+Symboles.ARROW_LITTLE_LEFT.get());
 
 		Random r = new Random();
-		int nbr = r.nextInt(3)+5;
+		List<Material> inInv = new ArrayList<>();
+
+		int nbr = r.nextInt(5)+8;
+		int pp = r.nextInt(2)+1;
 		int slot;
 
 		int isenchant;
@@ -91,56 +100,117 @@ public class NMSChicken extends EntityChicken implements Listener{
 		int lvlenchant;
 		int selected;
 
-		ItemStack dh = new ItemStack(Material.DIAMOND_HELMET);
-		ItemStack dc = new ItemStack(Material.DIAMOND_CHESTPLATE);
-		ItemStack dl = new ItemStack(Material.DIAMOND_LEGGINGS);
-		ItemStack db = new ItemStack(Material.DIAMOND_BOOTS);
-		
-		armors.add(dh);
-		armors.add(dc);
-		armors.add(dl);
-		armors.add(db);
-		
-		ItemStack bow = new ItemStack(Material.BOW);
-		ItemStack ds = new ItemStack(Material.DIAMOND_SWORD); 
-		
-		weapons.add(bow);
-		weapons.add(ds);
-		
-		ItemStack bread = new ItemStack(Material.BREAD, nbr*(nbr/2)+((nbr*2)/3));
-		ItemStack log = new ItemStack(Material.LOG, nbr*(nbr/2)+((nbr*2)/3));
-		ItemStack exp = new ItemStack(Material.EXP_BOTTLE, nbr*(nbr/2)+((nbr*2)/3));
-		ItemStack arrow = new ItemStack(Material.ARROW, nbr+(nbr/2)+5);
+		ItemStack ih = new ItemStack(Material.IRON_HELMET);
+		ItemStack ic = new ItemStack(Material.IRON_CHESTPLATE);
+		ItemStack il = new ItemStack(Material.IRON_LEGGINGS);
+		ItemStack ib = new ItemStack(Material.IRON_BOOTS);
 
+		armors.add(ih);
+		armors.add(ic);
+		armors.add(il);
+		armors.add(ib);
+
+		ItemStack bow = new ItemStack(Material.BOW);
+		ItemStack is = new ItemStack(Material.IRON_SWORD); 
+
+		weapons.add(bow);
+		weapons.add(is);
+
+		Potion h1 = new Potion(PotionType.INSTANT_HEAL, 1);
+		Potion h2 = new Potion(PotionType.INSTANT_HEAL, 2);
+		Potion po1 = new Potion(PotionType.POISON, 1, true);
+		Potion po2 = new Potion(PotionType.POISON, 2, true);
+		Potion f1 = new Potion(PotionType.STRENGTH, 1, true, true);
+		Potion f2 = new Potion(PotionType.STRENGTH, 2, true);
+		Potion w1 = new Potion(PotionType.WEAKNESS, 1, true, true);
+		Potion w2 = new Potion(PotionType.WEAKNESS, 2, true, true);
+		Potion r1 = new Potion(PotionType.REGEN, 2, true);
+
+		ItemStack p1 = h1.toItemStack(pp);
+		ItemStack p2 = h2.toItemStack(pp);
+		ItemStack p4 = po1.toItemStack(pp);
+		ItemStack p5 = po2.toItemStack(pp);
+		ItemStack p7 = f1.toItemStack(pp);
+		ItemStack p8 = f2.toItemStack(pp);
+		ItemStack p3 = w1.toItemStack(pp);
+		ItemStack p6 = w2.toItemStack(pp);
+		ItemStack p9 = r1.toItemStack(pp);
+
+		potions.add(p1);
+		potions.add(p2);
+		potions.add(p3);
+		potions.add(p4);
+		potions.add(p5);
+		potions.add(p6);
+		potions.add(p7);
+		potions.add(p8);
+		potions.add(p9);
+		
+		MonsterEggs me = new MonsterEggs(383, (byte)50);
+		ItemStack bread = new ItemStack(Material.BREAD, (nbr*(nbr/2)+((nbr*2)/3)/(pp*2))/pp);
+		ItemStack steak = new ItemStack(Material.COOKED_BEEF, (nbr*(nbr/2)+((nbr*2)/3)/(pp*2))/pp);
+		ItemStack iron = new ItemStack(Material.IRON_ORE, nbr+(nbr*2-pp*4)/4);
+		ItemStack gold = new ItemStack(Material.GOLD_ORE, (nbr+(nbr*2-pp*4))/4);
+		ItemStack coal = new ItemStack(Material.COAL, nbr+(nbr*2+2-pp*4+2)/2);
+		ItemStack obsi = new ItemStack(Material.OBSIDIAN, (pp*3)/2);
+		ItemStack creep = me.toItemStack(pp*pp+3);
+		ItemStack log = new ItemStack(Material.LOG, nbr*(nbr/2)+((nbr*2)/3)/(pp*2)/4);
+		ItemStack exp = new ItemStack(Material.EXP_BOTTLE, nbr*(pp/2)+((nbr-pp*2)/3));
+		ItemStack arrow = new ItemStack(Material.ARROW, nbr+(nbr/2)+5);
+		
 		items.add(bread);
 		items.add(log);
 		items.add(exp);
-		items.add(arrow);
+		items.add(steak);
+		items.add(iron);
+		items.add(creep);
+		items.add(gold);
+		items.add(coal);
+		items.add(obsi);
 		
+		noDouble.add(bread);
+		noDouble.add(log);
+		noDouble.add(exp);
+		noDouble.add(steak);
+		noDouble.add(iron);
+		noDouble.add(gold);
+		noDouble.add(coal);
+
 		items.addAll(weapons);
 		items.addAll(armors);
-		
+		items.addAll(potions);
+
 		for(int i = 0; i < nbr; i++){
 
 			isenchant = r.nextInt(armors.size());
 			whatenchant = r.nextInt(4)+1;
-			lvlenchant = r.nextInt(3)+1;
+			lvlenchant = r.nextInt(5);
 
 			selected = r.nextInt(items.size());
 			slot = r.nextInt(27);
 
 			ItemStack item = items.get(selected);
 
-			if(!this.chickenInv.contains(item)){
+			if(!inInv.contains(item.getType()) && !this.chickenInv.contains(item)){
 				if(armors.contains(item)){
-					if(isenchant == armors.size()/2){
-						Others.addEnchant(item, Enchantment.PROTECTION_ENVIRONMENTAL, 1);
+					inInv.add(item.getType());
+					if(isenchant > armors.size()/2 && isenchant < armors.size()){
+						if(whatenchant == 1){
+							Others.addEnchant(item, Enchantment.DURABILITY, lvlenchant);
+						}else if(whatenchant == 2){
+							Others.addEnchant(item, Enchantment.THORNS, lvlenchant);
+						}else{
+							Others.addEnchant(item, Enchantment.PROTECTION_ENVIRONMENTAL, lvlenchant);
+						}
 					}
 				}
 
 				if(weapons.contains(item)){
-					if(isenchant == weapons.size()/2){
+					inInv.add(item.getType());
+					if(isenchant > armors.size()/2 && isenchant < armors.size()){
 						if(item.getType().equals(Material.BOW)){
+							this.chickenInv.addItem(arrow);
+							inInv.add(arrow.getType());
 							if(whatenchant == 1){
 								Others.addEnchant(item, Enchantment.ARROW_FIRE, lvlenchant);
 							}else if(whatenchant == 2){
@@ -149,11 +219,21 @@ public class NMSChicken extends EntityChicken implements Listener{
 								Others.addEnchant(item, Enchantment.ARROW_DAMAGE, lvlenchant);
 							}
 						}else{
-							Others.addEnchant(item, Enchantment.DAMAGE_ALL, lvlenchant);
+							if(whatenchant == 1){
+								Others.addEnchant(item, Enchantment.FIRE_ASPECT, lvlenchant);
+							}else if(whatenchant == 2){
+								Others.addEnchant(item, Enchantment.KNOCKBACK, lvlenchant);
+							}else{
+								Others.addEnchant(item, Enchantment.DAMAGE_ALL, lvlenchant);
+							}
 						}
 					}
 				}
-
+				
+				if(noDouble.contains(item)){
+					inInv.add(item.getType());
+				}
+				
 				this.chickenInv.setItem(slot, item);
 			}
 

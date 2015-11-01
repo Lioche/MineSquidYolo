5f98d164dcbc9aca9a180564db34ae7c88a11548
Lioche <games@lioche.be>
2015-11-01 22:18:26 +0100

    Update Kill.java

--- Kill.java
+++ Kill.java
@@ -10,6 +10,7 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -21,6 +22,12 @@ import be.lioche.api.enums.Particles;
 import be.lioche.api.enums.Symboles;
 import be.lioche.api.packet.Holo;
 import be.lioche.compact.main.Main;
+
+import com.massivecraft.factions.Rel;
+import com.massivecraft.factions.entity.BoardColl;
+import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.factions.entity.MPlayer;
+import com.massivecraft.massivecore.ps.PS;
 
 public class Kill implements Listener{
 
@@ -62,8 +69,7 @@ public class Kill implements Listener{
 			if(e.getDamager() instanceof Player){	
 				Player k = (Player)e.getDamager();
 				p.setMaximumNoDamageTicks(Main.instance.getConfig().getInt("PvP-Ticks"));
-				double dmg = e.getDamage();
-				if(p.getHealth()-dmg <= 0){
+				if(p.getHealth() <= 0.0){
 					p.sendMessage(Main.prefix+"Il restait "+ be.lioche.api.main.Main.df.format(k.getHealth()/2) + " " + Symboles.HEARTH.get()+" a "+k.getName()+".");
 				}
 			}else{
@@ -90,57 +96,78 @@ public class Kill implements Listener{
 			ItemStack i = new ItemStack(Material.MONSTER_EGG, 1, (byte)50);
 			double dropTnT = 100 - Main.instance.getConfig().getInt("Drops.tnt");
 			double dropCreep = 100 - Main.instance.getConfig().getInt("Drops.creepers");
-			
+
 			if(Math.random() * 100 >= dropCreep){
 				c.getWorld().dropItemNaturally(c.getLocation(), i);
 			}
-			
+
 			if(Math.random() * 100 >= dropTnT){
 				c.getWorld().dropItemNaturally(c.getLocation(), new ItemStack(Material.TNT));
 			}
 		}
 	}
 
-	@EventHandler
+	@EventHandler(priority=EventPriority.LOW)
 	public void combo(EntityDamageByEntityEvent e){
 		if(e.getEntity() instanceof Player && e.getDamager() instanceof Player){
+
+
+
 			Player p = (Player)e.getEntity();
+			MPlayer mp = MPlayer.get(p);
+
 			Player k = (Player)e.getDamager();
+			MPlayer mk = MPlayer.get(k);
 
-			Kill killP = getPlayerKill(p, p.getUniqueId(), k);
-			Kill killK = getPlayerKill(k, k.getUniqueId(), p);
+			Faction fp = mp.getFaction(); // Faction mec tapé
+			Faction fk = mk.getFaction(); // Faction tapeur
 
-			if(killK.t.equals(p)){
+			Rel rpk = fp.getRelationWish(fk); // Relation fp et fk
 
-				killK.count = killK.count + 1;
-				killP.count = 0;
+			Faction lfp = BoardColl.get().getFactionAt(PS.valueOf(p.getLocation())); // Faction à l'endroit du tapé
+			Faction lfk = BoardColl.get().getFactionAt(PS.valueOf(k.getLocation())); // Faction à l'endroit du tapeur
 
-				if(killK.count  > 1){
-					Holo h = new Holo();
-					if(killK.count >= 10){
-						if(p.getFireTicks() != 0){
-							p.setFireTicks(killK.count);
+			if(!Main.isOnSpawn(p) && !Main.isOnSpawn(k) && e.getDamage() > 0 && !e.isCancelled()){
+				if(fp != fk && rpk != Rel.ALLY){ // Si les deux pas dans la même faction et si pas ally
+					if(lfp == fp && lfk != fp){ // Si le tapeur est pas dans la faction du tapé et tapé est dans sa faction
+						Kill killP = getPlayerKill(p, p.getUniqueId(), k);
+						Kill killK = getPlayerKill(k, k.getUniqueId(), p);
+
+						if(killK.t.equals(p)){
+
+							killK.count = killK.count + 1;
+							killP.count = 0;
+
+							if(killK.count  > 1){
+								Holo h = new Holo();
+								if(killK.count >= 10){
+									if(p.getFireTicks() != 0){
+										p.setFireTicks(killK.count);
+									}
+									for(double t = 0; t < 3.5 * Math.PI; t += 0.39){
+										Particles.sendParticle(EnumParticle.REDSTONE, k.getLocation(), Math.cos(t)/1.5, t/5, Math.sin(t)/1.5, 2, 5);
+										Particles.sendParticle(EnumParticle.SNOW_SHOVEL, k.getLocation(), Math.sin(t)/1.5, t/5, Math.cos(t)/1.5, 10, 25);
+									}
+
+									h.create(p.getLocation().add(0,1,0), "§6§lMEGA COMBOOO §e(§6§l"+killK.count+"§e)");
+									h.showToAll();
+									Holo.followPlayer(p, h, 1L);
+									k.getWorld().playSound(k.getLocation(), Sound.FIZZ, 1, 3);
+								}else{
+									h.create(p.getLocation().add(0,1,0), "§c§lCombo x"+killK.count);
+									h.showToAll();
+									Holo.followPlayer(p, h, 1L);
+								}
+							}
+
+						}else{
+							killK.t = p;
+							killK.count = 1;
+							killP.count = 0;
 						}
-						for(double t = 0; t < 3.5 * Math.PI; t += 0.39){
-							Particles.sendParticle(EnumParticle.REDSTONE, k.getLocation(), Math.cos(t)/1.5, t/5, Math.sin(t)/1.5, 2, 5);
-							Particles.sendParticle(EnumParticle.SNOW_SHOVEL, k.getLocation(), Math.sin(t)/1.5, t/5, Math.cos(t)/1.5, 10, 25);
-						}
 
-						h.create(p.getLocation().add(0,1,0), "§6§lMEGA COMBOOO §e(§6§l"+killK.count+"§e)");
-						h.showToAll();
-						Holo.followPlayer(p, h, 1L);
-						k.getWorld().playSound(k.getLocation(), Sound.FIZZ, 1, 3);
-					}else{
-						h.create(p.getLocation().add(0,1,0), "§c§lCombo x"+killK.count);
-						h.showToAll();
-						Holo.followPlayer(p, h, 1L);
 					}
 				}
-
-			}else{
-				killK.t = p;
-				killK.count = 1;
-				killP.count = 0;
 			}
 		}
 	}
